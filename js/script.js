b1142cea25d1c62b0a9ceff4c2a5b0ddbfc0f51b
Adil <Adil523@mail.ru>
2021-01-18 23:44:35 +0600

    WTF

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-        menuItem = document.querySelectorAll('.menu_item'),
-        hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu'),
+		menuItem = document.querySelectorAll('.menu_item'),
+		hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+	humburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('menu_active');
+	});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('menu_active');
+		})
+	})
 })
